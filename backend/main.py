2020b3c13b86b173b78ec9a495e6bf255ac8f05b
OswinDsilva <oswin.dsilva22@gmail.com>
2026-03-29 15:24:00 +0530

    backend router and middleware ordering fix?

--- backend/main.py
+++ backend/main.py
@@ -10,13 +10,6 @@ app = FastAPI(
     redoc_url="/redoc" if ENVIRONMENT == "dev" else None,
 )
 
-app.include_router(auth.router)
-app.include_router(categories.router)
-app.include_router(starting_balances.router)
-app.include_router(transactions.router)
-app.include_router(reports.router)
-
-
 origins = [
     "https://personal-expense-tracker-self-mu.vercel.app",
     "http://localhost",
@@ -34,6 +27,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(auth.router)
+app.include_router(categories.router)
+app.include_router(starting_balances.router)
+app.include_router(transactions.router)
+app.include_router(reports.router)
 
 
 @app.get("/")
