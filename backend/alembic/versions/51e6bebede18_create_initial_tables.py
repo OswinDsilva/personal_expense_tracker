e07d4ae456ab144380ee8d24a8cabbe1064d2573
OswinDsilva <oswin.dsilva22@gmail.com>
2026-08-21 14:07:40 +0530

    Lint

--- backend/alembic/versions/51e6bebede18_create_initial_tables.py
+++ backend/alembic/versions/51e6bebede18_create_initial_tables.py
@@ -7,9 +7,8 @@ Create Date: 2026-08-21 12:45:14.134341
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '51e6bebede18'
